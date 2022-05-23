252ea2ad4687ee71500be856233e9616fd3fe8fa
Kirill Koshelev <olegartor7@gmail.com>
2022-05-23 10:54:35 +0300

    build: update configs

--- apps/client/pages/api/auth/me.ts
+++ apps/client/pages/api/auth/me.ts
@@ -1,6 +1,6 @@
+import { NextApiRequest, NextApiResponse } from 'next'
 import { ironOptions } from 'features/auth'
 import { withIronSessionApiRoute } from 'iron-session/next'
-import { NextApiRequest, NextApiResponse } from 'next'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { method } = req
